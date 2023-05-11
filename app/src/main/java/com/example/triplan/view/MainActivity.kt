a2a8dc019b8e7ab62d720daf1ffa513a5502215d
Eungyu-code <Agyu625@gmail.com>
2023-05-11 21:10:30 +0900

    fix: add code about login(button)

--- app/src/main/java/com/example/triplan/view/MainActivity.kt
+++ app/src/main/java/com/example/triplan/view/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.triplan.view
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -14,10 +15,17 @@ class MainActivity : AppCompatActivity() {
 
 
         val gptBtn:Button=findViewById(R.id.gptBtn1)
+        val loginBtn:Button=findViewById(R.id.loginBtn1)
 
         gptBtn.setOnClickListener{
             val nextIntent=Intent(this,GptAskActivity::class.java)
             startActivity(nextIntent)
+        }
+
+        loginBtn.setOnClickListener{
+            val url="https://kauth.kakao.com/oauth/authorize?client_id=aa2c572bd90a086842d6c97ea2985589&redirect_uri=http://localhost:8080/user/login/kakao-callback&response_type=code"
+            val intent=Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            startActivity(intent)
         }
     }
 }
