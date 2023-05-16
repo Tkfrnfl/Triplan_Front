b5592d65d7829cc9e6f1f29c909772db5d58abb4
Eungyu-code <Agyu625@gmail.com>
2023-05-16 22:13:44 +0900

    feat: 카카오 로그인을 위해 애플리케이션 실행 시마다 네이티브 앱 키를 초기화하는 클래스 추가

---
+++ app/src/main/java/com/example/triplan/KakaoApplication.kt
@@ -0,0 +1,14 @@
+package com.example.triplan
+
+import android.app.Application
+import com.kakao.sdk.common.KakaoSdk
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class KakaoApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        KakaoSdk.init(this, "2af358120f051fdcfe738ee3768e7d71")
+    }
+}
