60e2d0aaa532bbeb9e50e2d4e662e4f125a895b6
Eungyu-code <Agyu625@gmail.com>
2023-05-16 22:15:53 +0900

    update: loginBtn의 ViewId 변경

--- app/src/main/java/com/example/triplan/view/MainActivity.kt
+++ app/src/main/java/com/example/triplan/view/MainActivity.kt
@@ -1,34 +1,35 @@
 package com.example.triplan.view
 
+import android.content.Context
 import android.content.Intent
-import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.EditText
 import com.example.triplan.R
 import dagger.hilt.android.AndroidEntryPoint
 
 
+
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
         val gptBtn:Button=findViewById(R.id.gptBtn1)
-        val loginBtn:Button=findViewById(R.id.loginBtn1)
 
         gptBtn.setOnClickListener{
-            val nextIntent=Intent(this,GptAskActivity::class.java)
+            val nextIntent= Intent(this,GptAskActivity::class.java)
             startActivity(nextIntent)
         }
 
+        val loginBtn:Button=findViewById(R.id.loginBtn)
+
         loginBtn.setOnClickListener{
-            val url="https://kauth.kakao.com/oauth/authorize?client_id=aa2c572bd90a086842d6c97ea2985589&redirect_uri=http://localhost:8080/user/login/kakao-callback&response_type=code"
-            val intent=Intent(Intent.ACTION_VIEW, Uri.parse(url))
-            startActivity(intent)
+            val nextIntent = Intent(this, LoginActivity::class.java)
+            startActivity(nextIntent)
         }
+
     }
 }
