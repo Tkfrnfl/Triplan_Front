a902c959765d05c921be13bc926ce833d07046e7
Eungyu-code <Agyu625@gmail.com>
2023-05-16 22:17:05 +0900

    feat: 로그인 로직 추가

--- app/src/main/java/com/example/triplan/view/LoginActivity.kt
+++ app/src/main/java/com/example/triplan/view/LoginActivity.kt
@@ -1,12 +1,81 @@
 package com.example.triplan.view
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.apollographql.apollo3.ApolloCall
+import com.apollographql.apollo3.ApolloClient
+import com.apollographql.apollo3.api.ApolloResponse
+import com.apollographql.apollo3.exception.ApolloException
+import com.example.triplan.LoginMutation
+import com.example.triplan.R
+import com.kakao.sdk.user.UserApiClient
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+import okhttp3.*
+import okhttp3.RequestBody.Companion.toRequestBody
+import okio.BufferedSink
+import okio.IOException
 
+@AndroidEntryPoint
 class LoginActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
 
+        val kakaoLoginBtn: Button = findViewById(R.id.kakaoLoginButton)
+
+        kakaoLoginBtn.setOnClickListener {
+            UserApiClient.instance.loginWithKakaoAccount(this) { token, error ->
+                if (token != null) {
+                    saveAccessToken(token.accessToken)
+                    sendRequestWithAccessToken()
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                } else if (error != null) {
+                    Log.e("Error", "$error")
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+            }
+        }
+    }
+
+    private fun sendRequestWithAccessToken() {
+
+        val apolloClient = ApolloClient.Builder()
+            .serverUrl("http://10.0.2.2:8080/graphql")
+            .build()
+
+        runBlocking {
+            try {
+                val response: ApolloResponse<LoginMutation.Data> = apolloClient.mutation(LoginMutation()).execute()
+                val data = response.data.toString()
+
+                Log.v("Response", "data")
+            } catch (e: ApolloException) {
+                throw ApolloException("통신 실패 에러")
+            }
+        }
+    }
+
+    private fun getAccessToken(): String? {
+        val sharedPreferences = getSharedPreferences("", Context.MODE_PRIVATE)
+        return sharedPreferences.getString("accessToken", null)
+    }
+
+    private fun saveAccessToken(accessToken: String) {
+        val sharedPreferences = getSharedPreferences("", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putString("accessToken", accessToken)
+        editor.apply()
     }
 }
